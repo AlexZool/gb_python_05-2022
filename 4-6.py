ff8e27e303e945f9a96acf3f756c2a2cbf101f81
AlexZool <46959340+AlexZool@users.noreply.github.com>
2022-06-20 16:17:08 +0300

    Add files via upload

---
+++ 4-6.py
@@ -0,0 +1,14 @@
+from itertools import cycle, count
+
+for i in count(3):
+    print(i)
+    if i >= 10:
+        break
+
+i = 0
+my_list = ['a', 'b', 'c']
+for el in cycle(my_list):
+    print(el)
+    i += 1
+    if i >= 10:
+        break
